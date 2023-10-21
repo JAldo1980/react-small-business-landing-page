c6cb9964d5f7c3cea462449893e038ddc9aac801
JAldo1980 <jamesalderman1980@gmail.com>
2023-10-21 15:33:19 +0100

    imported functions

--- glow-1/src/LinkedinPackageData.jsx
+++ glow-1/src/LinkedinPackageData.jsx
@@ -1,33 +1,16 @@
+import {
+  BronzePackage,
+  SilverPackage,
+  GoldPackage,
+} from "./LinkedInPackageHtml"; // Import your package components
+
 export default [
   {
     id: 1,
     headline: "LinkedIn Bronze",
     price: 50,
     text: "This is text for the Bronze package pricing headline. Here is where I will detail the offers etc.",
-    html: `
-      <div>
-        <ul>
-          <li>
-            <span>Profile Critique:</span> A comprehensive review of your existing LinkedIn profile with actionable feedback.
-          </li>
-          <li>
-            <span>Keyword Optimization:</span> Strategic placement of relevant keywords to improve your profile's visibility.
-          </li>
-          <li>
-            <span>Headline Enhancement:</span> Crafting an attention-grabbing headline to make a strong first impression.
-          </li>
-          <li>
-            <span>Custom URL Creation:</span> Personalized LinkedIn URL for a professional touch.
-          </li>
-          <li>
-            <span>Networking Tips:</span> Recommendations for expanding your professional network.
-          </li>
-          <li>
-            <span>30-Minute Consultation:</span> A brief consultation session to discuss your profile improvements.
-          </li>
-        </ul>
-      </div>
-    `,
+    html: <BronzePackage />,
     button: "https://www.linkedin.com/in/jamespalderman/",
     stock: 5,
   },
@@ -35,29 +18,8 @@ export default [
     id: 2,
     headline: "LinkedIn Sliver",
     price: 100,
-    text: "This is text for the Bronze package pricing headline. Here is where I will detail the offers etc.",
-    html: `
-<div>
-    <p>Everything in the Bronze Package, plus:</p>
-    <ul>
-        <li>
-            <span>Profile Optimization:</span> An in-depth overhaul of your LinkedIn profile, including rewriting the About section and updating your experience.
-        </li>
-        <li>
-            <span>Customized Cover Photo:</span> Designing a personalized banner to enhance your brand image.
-        </li>
-        <li>
-            <span>Skills and Endorsements:</span> Recommendations on skills to highlight and how to secure endorsements.
-        </li>
-        <li>
-            <span>Content Strategy Guidance:</span> Tips for sharing valuable content and engaging with your network.
-        </li>
-        <li>
-            <span>60-Minute Consultation:</span> A more extensive consultation session for a deeper understanding of your profile and goals.
-        </li>
-    </ul>
-</div>
-`,
+    text: "This is text for the Silver package pricing headline. Here is where I will detail the offers etc.",
+    html: <SilverPackage />,
     button: "https://www.linkedin.com/in/jamespalderman/",
     stock: 5,
   },
@@ -65,29 +27,8 @@ export default [
     id: 3,
     headline: "LinkedIn Gold",
     price: 150,
-    text: "This is text for the Bronze package pricing headline. Here is where I will detail the offers etc.",
-    html: `
-    <div>
-        <p>Everything in the Silver Package, plus:</p>
-        <ul>
-            <li>
-                <span>Premium Profile Rewrite:</span> A complete rewrite and enhancement of your LinkedIn profile, making it stand out among your peers.
-            </li>
-            <li>
-                <span>In-Depth Content Strategy:</span> A comprehensive content strategy that includes post creation, publishing schedule, and content ideas tailored to your niche.
-            </li>
-            <li>
-                <span>Connection Growth Strategy:</span> Strategies for expanding your network with targeted connections.
-            </li>
-            <li>
-                <span>Recommendation Guidance:</span> Advice on how to seek and receive compelling recommendations.
-            </li>
-            <li>
-                <span>90-Minute Consultation:</span> A thorough consultation session to align your LinkedIn profile with your long-term objectives.
-            </li>
-        </ul>
-    </div>
-    `,
+    text: "This is text for the Gold package pricing headline. Here is where I will detail the offers etc.",
+    html: <GoldPackage />,
     button: "https://www.linkedin.com/in/jamespalderman/",
     stock: 5,
   },
