779bb5f27f9a8da0da9795bfa09437281a559258
JAldo1980 <jamesalderman1980@gmail.com>
2023-10-22 10:36:53 +0100

    update jsx files

--- glow-1/src/WebsitePackageData.jsx
+++ glow-1/src/WebsitePackageData.jsx
@@ -1,9 +1,16 @@
+import {
+  BronzeWebPackage,
+  GoldWebPackage,
+  SilverWebPackage,
+} from "./WebsitePackageHtml";
+
 export default [
   {
     id: 1,
     headline: "Website Bronze",
     price: 50,
     text: "This is text for the Bronze package pricing headline. Here is where I will detail the offers etc.",
+    html: <BronzeWebPackage />,
     button: "https://www.linkedin.com/in/jamespalderman/",
     stock: 5,
   },
@@ -12,6 +19,7 @@ export default [
     headline: "Website Sliver",
     price: 100,
     text: "This is text for the Silver package pricing headline. Here is where I will detail the offers etc.",
+    html: <SilverWebPackage />,
     button: "https://www.linkedin.com/in/jamespalderman/",
     stock: 2,
   },
@@ -20,6 +28,7 @@ export default [
     headline: "Website Gold",
     price: 150,
     text: "This is text for the Gold package pricing headline. Here is where I will detail the offers etc.",
+    html: <GoldWebPackage />,
     button: "https://www.linkedin.com/in/jamespalderman/",
     stock: 11,
   },
--- glow-1/src/Why.jsx
+++ glow-1/src/Why.jsx
@@ -7,7 +7,21 @@ export default function Why() {
         <img className="james-why-image" src="public/james-image.png" alt="" />
         <div className="why-cta">
           <h3>Why Glow?</h3>
-          <p>This is where I talk about my expertise etc...</p>
+          <p>
+            Hi, I am James. In today's digital age, personal branding is not
+            just a professional option; it's a necessity. A strong personal
+            brand on LinkedIn can elevate your online presence, making you more
+            visible and credible to a vast professional network.
+          </p>
+          <p>
+            It enhances your networking capabilities, establishes you as an
+            industry authority, and can open doors to exciting career
+            opportunities. By harnessing the power of personal branding on
+            LinkedIn, you not only enhance your professional journey but also
+            create a compelling narrative for your website marketing material,
+            underlining your expertise and credibility for potential clients,
+            partners, and employers.
+          </p>
           <button className="btn">Order</button>
         </div>
       </div>
